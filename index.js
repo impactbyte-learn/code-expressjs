5f244d12fd5a072bafa8bd80efb1c4a8a3703455
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-03 10:53:06 +0700

    Add endpoint for get todos by id

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(bodyParser.json());
 app.get("/", controllers.displayHelloWorld)
 app.post("/", controllers.displayName)
 
-app.get("/todos", todos.display)
+app.get("/todos", todos.get)
+app.get("/todos/:id", todos.getOneById)
 app.post("/todos", todos.add)
 
 app.listen(PORT, () => {
